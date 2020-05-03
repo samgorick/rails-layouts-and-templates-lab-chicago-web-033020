4b234eb6c7dd5d880ea8e7bfc46500b764c414c0
gozmeister <samgorick.sg@gmail.com>
2020-05-03 17:16:47 -0500

    Done.

---
+++ app/controllers/store_admin_controller.rb
@@ -0,0 +1,15 @@
+class StoreAdminController < ApplicationController
+  layout 'admin', only: [:home]
+  
+
+  def home
+  end
+
+  def orders
+    render :layout => 'order_administration'
+  end
+
+  def invoice
+    render :layout => false
+  end
+end
